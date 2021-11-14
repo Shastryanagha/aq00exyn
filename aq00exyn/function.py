737c562b23e9eb280448087100b8395d31a8bcac
Shastryanagha <94296925+Shastryanagha@users.noreply.github.com>
2021-11-14 19:20:43 +0530

    Function code updated

--- aq00exyn/function.py
+++ aq00exyn/function.py
@@ -3,6 +3,4 @@ from ipywidgets import interact, fixed
 from PIL import Image
  
 def imshow(X, resize=None):
-	img = cv2.imread(X)
-	res = cv2.resize(img, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
-        return res
+	imageB_array = resize(X, (100, 500), anti_aliasing=True)
