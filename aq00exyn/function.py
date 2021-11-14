42f7b7d43afc7a740ba2e8296f9009eb06e414b6
Shastryanagha <94296925+Shastryanagha@users.noreply.github.com>
2021-11-14 19:21:52 +0530

    updated code

--- aq00exyn/function.py
+++ aq00exyn/function.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
+from skimage.transform import resize
  
 def imshow(X, resize=None):
 	imageB_array = resize(X, (100, 500), anti_aliasing=True)
